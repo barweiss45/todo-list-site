0cac74e943bb73a2305c490128d9e6377583ca07
Barry Weiss <bweiss@pegasusnet.ai>
2024-02-24 18:08:54 -0500

    Add factory function for Flask

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+"""
+Factory Function for Flask Application
+"""
+
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+
+
+def create_app():
+    """Factory Function for Flask Application"""
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+    db.init_app(app)
+
+    with app.app_context():
+        from models import TaskModel  # Import models here to register them with SQLAlchemy
+        
+        # Import Blueprints and register them
+        from .views import task_blueprint
+        app.register_blueprint(task_blueprint)
+
+    return app
