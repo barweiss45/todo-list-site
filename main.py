bd9627e409b0868b0ddde9e5f625d625fc00393e
Barry Weiss <bweiss@pegasusnet.ai>
2024-02-24 19:48:07 -0500

    Removed app/server.py and created main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+"""Entrypoint for application"""
+
+from app import create_app
+
+app = create_app()  # Calls Factory Function
+
+if __name__ == '__main__':
+    app.run(debug=True)
