047d164e2605f414113802267a71c64e89e48eec
Barry Weiss <bweiss@pegasusnet.ai>
2024-02-24 20:36:33 -0500

    Refactor the for correct Model names

--- app/views/routes.py
+++ app/views/routes.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from flask import jsonify, redirect, render_template, request, url_for
+from app.models import TaskModel, CreateTask
 
 from .. import db
 from . import task_blueprint
@@ -11,16 +12,15 @@ def index():
     return render_template('index.html')
 
 
-@task_blueprint.route("/create_task", method=["POST"])
+@task_blueprint.route("/create_task", methods=["POST"])
 def create_task():
     try:
-        task_data = Task(**request.json)  # Validate incoming JSON data with Pydantic
+        task_data = CreateTask(**request.json)  # Validate incoming JSON data with Pydantic
         new_task = TaskModel(task=task_data.task, completed=task_data.completed)  # Create an SQLAlchemy model instance
         db.session.add(new_task)
         db.session.commit()
-        return jsonify({"success": True, "task": task_data.dict()}), 201
+        return jsonify({"success": True, "task": task_data.model_dump()}), 201
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
- 
 
 # Add more routes as needed
