cc61f658ac8483a9817b5fbc5dbe5b7fff8706c1
Barry Weiss <bweiss@pegasusnet.ai>
2024-02-24 19:49:20 -0500

    Refactor app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ Factory Function for Flask Application
 
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -21,8 +21,8 @@ def create_app():
     db.init_app(app)
 
     with app.app_context():
-        from models import TaskModel  # Import models here to register them with SQLAlchemy
-        
+        from app.models import TaskModel  # Import models here to register them with SQLAlchemy
+
         # Import Blueprints and register them
         from .views import task_blueprint
         app.register_blueprint(task_blueprint)
