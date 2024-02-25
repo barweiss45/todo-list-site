b4ba8875ac436c372599775963370157330e54eb
Barry Weiss <bweiss@pegasusnet.ai>
2024-02-24 20:39:09 -0500

    Add example to TaskModel

--- app/models/task.py
+++ app/models/task.py
@@ -4,6 +4,16 @@ from .. import db
 
 
 class TaskModel(db.Model):
+    """
+    TaskModel - Database Schema
+
+    Example:
+{
+"id": 1,
+"task": "Complete this todo application.",
+"completed": False
+}
+    """
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean, default=False)
