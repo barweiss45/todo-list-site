8124942b48558855199a3f508349666080f2693e
Barry Weiss <bweiss@pegasusnet.ai>
2024-02-25 20:59:08 -0500

    add complete endpoint

--- app/views/routes.py
+++ app/views/routes.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from flask import jsonify, redirect, render_template, request, url_for
-from app.models import TaskModel, CreateTask
+from app.models import TaskModel, CreateTask, CompleteTask, ValidationError, ClientResponse
 
 from .. import db
 from . import task_blueprint
@@ -13,17 +13,31 @@ def index():
     return render_template('index.html')
 
 
-@task_blueprint.route("/create_task", methods=["POST"])
-def create_task():
+@task_blueprint.route("/tasks/create", methods=["POST"])
+def create():
     try:
         task_data = CreateTask(**request.json)  # Validate incoming JSON data with Pydantic
-        new_task = TaskModel(task=task_data.task, completed=task_data.completed)  # Create an SQLAlchemy model instance
+        new_task = TaskModel(task=task_data.task,
+                             completed=task_data.completed)  # Create an SQLAlchemy model instance
         db.session.add(new_task)
         db.session.commit()
         logger.info("/create_task - R_IP: %s",  request.remote_addr)
-        return jsonify({"success": True, "task": task_data.model_dump()}), 201
-    except ValueError as e:
+        return jsonify({"success": True, "message": task_data.model_dump()}), 201
+    except (ValueError, ValidationError) as e:
         logger.error(e)
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"success": False, "message": str(e)}), 400
+
+
+@task_blueprint.route("/tasks/<int:id>/complete", methods=["PUT"])
+def complete(id: int) -> ClientResponse:
+    try:
+        task_data = CompleteTask(**request.json)  # Validate Pydantic model
+    except ValidationError as e:
+        return jsonify({"succcess": False, "message": str(e)}), 400
+    task = TaskModel.query.get_or_404(id)
+    task.completed = task_data.complete_data
+
+    return jsonify({"success": True, "message": "Created"}), 200
+
 
 # Add more routes as needed
